5d01997543d0048b6f127db1ace528408e064814
eagle20 <68212776+eagle20@users.noreply.github.com>
2024-07-16 14:07:46 -0700

    Update app.py
    Test

--- app.py
+++ app.py
@@ -1,7 +1,20 @@
 from flask import Flask
 from flask_sock import Sock
-app = Flask(__name__)
 
-@app.route('/')
+INCOMING_CALL_ROUTE = '/'
+WEBSOCKET_ROUTE = '/realtime'
+
+app = Flask(__name__)
+sock = Sock(app)
+
+
+@app.route(INCOMING_CALL_ROUTE)
+def receive_call():
+    return "Real time phone call transcription app"
+
+@sock.route(WEBSOCKET_ROUTE)
+def transcription_websocket(ws):
+    pass
+    
 def hello_world():
     return 'Hello, World!'
