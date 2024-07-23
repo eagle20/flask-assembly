b08404a4dcc52d443b6ea0482442037dc6edde46
eagle20 <68212776+eagle20@users.noreply.github.com>
2024-07-23 10:51:39 -0700

    Update app.py

--- app.py
+++ app.py
@@ -80,7 +80,7 @@ def transcription_websocket(ws):
                 transcriber.stream(payload_mulaw)
             case "stop":
                 print('twilio stopped')
-                print("Final Final:", transcriber.full_transcript)
+                print("Final Final:", transcriber.final_transcript)
                 transcriber.close()
                 print('transcriber closed')
     
