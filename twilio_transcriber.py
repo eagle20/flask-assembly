58d00d6f250ab7217e6cf855b977377d5520de0d
eagle20 <68212776+eagle20@users.noreply.github.com>
2024-07-23 10:24:23 -0700

    Update twilio_transcriber.py

--- twilio_transcriber.py
+++ twilio_transcriber.py
@@ -7,6 +7,7 @@ load_dotenv()
 aai.settings.api_key = os.getenv('ASSEMBLYAI_API_KEY')
 
 TWILIO_SAMPLE_RATE = 8000 # Hz
+final_transcript = []
 
 def on_open(session_opened: aai.RealtimeSessionOpened):
     "Called when the connection has been established."
@@ -19,7 +20,7 @@ def on_data(transcript: aai.RealtimeTranscript):
         return
 
     if isinstance(transcript, aai.RealtimeFinalTranscript):
-        #final_transcript.append(transcript.text)
+        final_transcript.append(transcript.text)
         print(transcript.text, end="\r\n")
     #else:
         #print(transcript.text, end="\r")
@@ -32,14 +33,13 @@ def on_error(error: aai.RealtimeError):
 
 def on_close():
     "Called when the connection has been closed."
-    #full_transcript = "".join(final_transcript)
-    #print("Final Transcript:", full_transcript)
+    full_transcript = "".join(final_transcript)
+    print("Final Transcript:", full_transcript)
     print("Closing Session")
 
 
 class TwilioTranscriber(aai.RealtimeTranscriber):
     def __init__(self):
-        final_transcript = []
         super().__init__(
             on_data=on_data,
             on_error=on_error,
@@ -48,3 +48,4 @@ class TwilioTranscriber(aai.RealtimeTranscriber):
             sample_rate=TWILIO_SAMPLE_RATE,
             encoding=aai.AudioEncoding.pcm_mulaw
         )
+        self.final_transcript = final_transcript
