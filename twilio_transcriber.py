40ab50530964346e17c4e02263436284b760742a
eagle20 <68212776+eagle20@users.noreply.github.com>
2024-07-23 08:59:34 -0700

    Update twilio_transcriber.py

--- twilio_transcriber.py
+++ twilio_transcriber.py
@@ -19,7 +19,7 @@ def on_data(transcript: aai.RealtimeTranscript):
         return
 
     if isinstance(transcript, aai.RealtimeFinalTranscript):
-        self.final_transcript.append(transcript.text)
+        final_transcript.append(transcript.text)
         print(transcript.text, end="\r\n")
     #else:
         #print(transcript.text, end="\r")
@@ -30,9 +30,9 @@ def on_error(error: aai.RealtimeError):
     print("An error occured:", error)
 
 
-def on_close(self):
+def on_close():
     "Called when the connection has been closed."
-    full_transcript = "".join(self.final_transcript)
+    full_transcript = "".join(final_transcript)
     print("Final Transcript:", full_transcript)
     print("Closing Session")
 
