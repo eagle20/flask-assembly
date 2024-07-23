2fce55dd6b369b32cb0bc3e2b519b8b6943c6d27
eagle20 <68212776+eagle20@users.noreply.github.com>
2024-07-23 09:32:12 -0700

    Update twilio_transcriber.py

--- twilio_transcriber.py
+++ twilio_transcriber.py
@@ -8,39 +8,38 @@ aai.settings.api_key = os.getenv('ASSEMBLYAI_API_KEY')
 
 TWILIO_SAMPLE_RATE = 8000 # Hz
 
-def on_open(self, session_opened: aai.RealtimeSessionOpened):
+def on_open(session_opened: aai.RealtimeSessionOpened):
     "Called when the connection has been established."
     print("Session ID:", session_opened.session_id)
 
 
-def on_data(self, transcript: aai.RealtimeTranscript):
+def on_data(transcript: aai.RealtimeTranscript):
     "Called when a new transcript has been received."
     if not transcript.text:
         return
 
     if isinstance(transcript, aai.RealtimeFinalTranscript):
-        self.final_transcript.append(transcript.text)
+        final_transcript.append(transcript.text)
         print(transcript.text, end="\r\n")
     #else:
         #print(transcript.text, end="\r")
 
 
-def on_error(self, error: aai.RealtimeError):
+def on_error(error: aai.RealtimeError):
     "Called when the connection has been closed."
     print("An error occured:", error)
 
 
-def on_close(self):
+def on_close():
     "Called when the connection has been closed."
-    full_transcript = "".join(self.final_transcript)
+    full_transcript = "".join(final_transcript)
     print("Final Transcript:", full_transcript)
-    self.close()
     print("Closing Session")
 
 
 class TwilioTranscriber(aai.RealtimeTranscriber):
     def __init__(self):
-        self.final_transcript = []
+        final_transcript = []
         super().__init__(
             on_data=on_data,
             on_error=on_error,
